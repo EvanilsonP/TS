850f7b3dbab6740e9a85feec178b10675b82003b
EvanilsonP <pevanilson99@gmail.com>
2022-11-09 12:02:46 -0300

    Array types

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-// const person: {
-//     name: string,
-//     age: string
-// } = { 
+// // const person: {
+// //     name: string,
+// //     age: string
+// // } = { 
 var person = {
     name: 'Evanilson',
-    age: 23
+    age: 23,
+    hobbies: ['Sports', 'Cooking']
 };
+var favoriteActivities;
+favoriteActivities = ['Learn languages', 'Read'];
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map()) -> ERROR!!!
+}
 console.log(person.name);
--- app.ts
+++ app.ts
@@ -2,8 +2,17 @@
 // //     name: string,
 // //     age: string
 // // } = { 
-//     const person = {
-//         name: 'Evanilson',
-//         age: 23
-// }
-// console.log(person.name);
+    const person = {
+        name: 'Evanilson',
+        age: 23,
+        hobbies: ['Sports', 'Cooking']
+}
+let favoriteActivities: string[];
+favoriteActivities = ['Learn languages', 'Read'];
+
+for(const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map()) -> ERROR!!!
+}
+
+console.log(person.name);
