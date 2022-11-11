f67eb0c5053d7d2abf0b447f2db34a44e89ed9e6
EvanilsonP <pevanilson99@gmail.com>
2022-11-11 11:55:37 -0300

    Literal types

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
+    // if(resultConversion === 'as-number') {
+    //     return +result;
+    // } else {
+    //     return result.toString();
+    // }
 }
 ;
-var combinedAges = combine(30, 50);
-var combinedNames = combine('Max', 'Ajax');
+var combinedAges = combine(30, 50, 'as-number');
+var combinedSTRINGSAges = combine('30', '50', 'as-number');
+var combinedNames = combine('Max', 'Ajax', 'as-text');
 console.log(combinedAges);
+console.log(combinedSTRINGSAges);
 console.log(combinedNames);
--- app.ts
+++ app.ts
@@ -1,15 +1,22 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-text') {
     let result;
-    if(typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if(typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
     } else {
         result = input1.toString() + input2.toString();
     }
-     return result;
+    return result;
+    // if(resultConversion === 'as-number') {
+    //     return +result;
+    // } else {
+    //     return result.toString();
+    // }
 };
 
-const combinedAges = combine(30, 50);
-const combinedNames = combine('Max', 'Ajax');
+const combinedAges = combine(30, 50, 'as-number');
+const combinedSTRINGSAges = combine('30', '50', 'as-number');
+const combinedNames = combine('Max', 'Ajax', 'as-text');
 
 console.log(combinedAges);
+console.log(combinedSTRINGSAges);
 console.log(combinedNames);
