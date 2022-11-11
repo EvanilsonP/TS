64115cdbc69af55ca3b2a91d21cd5f459571783c
EvanilsonP <pevanilson99@gmail.com>
2022-11-11 12:55:03 -0300

    Function Types and callbacks

--- app.js
+++ app.js
@@ -6,7 +6,16 @@ function printResult(num) {
     console.log('Result is: ' + num);
 }
 ;
+function addAndHabdle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+;
+addAndHabdle(10, 20, function (result) {
+    console.log(result);
+});
 printResult(add(30, 50));
+// Function as types;
 var combineValues;
 combineValues = add;
 console.log(combineValues(10, 15));
--- app.ts
+++ app.ts
@@ -6,6 +6,16 @@ function printResult(num: number): void {
     console.log('Result is: ' + num);
 };
 
+// Function Types and callbacks
+function addAndHabdle(n1: number, n2: number, cb: (num: number) => void) {
+    const result = n1+n2;
+    cb(result);
+};
+
+addAndHabdle(10, 20, (result) => {
+    console.log(result);
+});
+
 printResult(add(30, 50));
 // Function as types;
 let combineValues: (a: number, b: number) => number;
