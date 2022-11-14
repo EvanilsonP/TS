0e1a4156e826052149beef33f55a9c82e1774a7b
EvanilsonP <pevanilson99@gmail.com>
2022-11-14 11:29:24 -0300

    Private and Public acess modifiers

--- dist/4-Classes&Interfaces/app.js
+++ dist/4-Classes&Interfaces/app.js
@@ -1,14 +1,27 @@
 "use strict";
 class Department {
     constructor(name) {
+        this.employees = [];
         this.name = name;
     }
     describe() {
         console.log('department: ' + this.name);
     }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
+;
 const accounting = new Department('Accouting');
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+accounting.name = 'DUMMY';
 accounting.describe();
-const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
-accountingCopy.describe();
+accounting.printEmployeeInformation();
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+// accountingCopy.describe();
 //# sourceMappingURL=app.js.map
