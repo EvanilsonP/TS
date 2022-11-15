5163a3cd7262cabf8584a112e02eb7ec90dba888
EvanilsonP <pevanilson99@gmail.com>
2022-11-15 12:42:41 -0300

    Optional parameters & properties

--- 4-Classes&Interfaces/app.ts
+++ 4-Classes&Interfaces/app.ts
@@ -1,39 +1,47 @@
+// type AddFn = (a: number, b: number) => number;
 interface AddFn {
     (a: number, b: number): number;
-}
-
-let add_: AddFn;
-
-add_ = (n1: number, n2: number) => {
+  }
+  
+  let add: AddFn;
+  
+  add = (n1: number, n2: number) => {
     return n1 + n2;
-};
-
-interface Named {
-    name: string;
-};
-
-interface Greetable extends Named {
-    readonly name: string;
-    // age: number;
+  };
+  
+  interface Named {
+    readonly name?: string;
+    outputName?: string;
+  }
+  
+  interface Greetable extends Named {
     greet(phrase: string): void;
-};
-
-class Person implements Greetable {
-    name: string;
+  }
+  
+  class Person implements Greetable {
+    name?: string;
     age = 30;
-    
-    constructor(name: string) {
-        this.name = name;
+  
+    constructor(n?: string) {
+      if (n) {
+        this.name = n;
+      }
     }
+  
     greet(phrase: string) {
-        console.log(phrase + '' + this.name);
+      if (this.name) {
+        console.log(phrase + ' ' + this.name);
+      } else {
+        console.log('Hi!');
+      }
     }
-};
-
-let user1: Greetable;
-
-user1 = new Person('Evanilson P');
-// user1.name = 'Evanilson' //  NOPE!
-console.log(user1);
-
-user1.greet('Hi there! I am ');
+  }
+  
+  let user1: Greetable;
+  
+  user1 = new Person();
+  // user1.name = 'Manu';
+  
+  user1.greet('Hi there - I am');
+  console.log(user1);
+  
--- dist/4-Classes&Interfaces/app.js
+++ dist/4-Classes&Interfaces/app.js
@@ -1,23 +1,27 @@
 "use strict";
-let add_;
-add_ = (n1, n2) => {
+let add;
+add = (n1, n2) => {
     return n1 + n2;
 };
-;
-;
 class Person {
-    constructor(name) {
+    constructor(n) {
         this.age = 30;
-        this.name = name;
+        if (n) {
+            this.name = n;
+        }
     }
     greet(phrase) {
-        console.log(phrase + '' + this.name);
+        if (this.name) {
+            console.log(phrase + ' ' + this.name);
+        }
+        else {
+            console.log('Hi!');
+        }
     }
 }
-;
 let user1;
-user1 = new Person('Evanilson P');
-// user1.name = 'Evanilson' //  NOPE!
+user1 = new Person();
+// user1.name = 'Manu';
+user1.greet('Hi there - I am');
 console.log(user1);
-user1.greet('Hi there! I am ');
 //# sourceMappingURL=app.js.map
