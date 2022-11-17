6e9e609cf38b61fe37e66e35a2d0c16688f79094
EvanilsonP <pevanilson99@gmail.com>
2022-11-17 15:31:05 -0300

    Working with constraints and another generic function

--- 6-Generics/app.ts
+++ 6-Generics/app.ts
@@ -18,3 +18,19 @@ function merge<T extends Object, U extends Object>(objA: T, objB: U) {
 
 const mergedObj = merge<{name: string, hobbies: string[]}, {age: number}>({name: 'Evanilson', hobbies: ['Sports']}, {age: 23});
 console.log(mergedObj.age);
+
+// CREATING ANOTHER GENERIC FUNCTION
+interface Lengthy{
+    length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+    let descriptionText = 'Got no value';
+    if(element.length === 1) {
+        descriptionText = 'Got 1 element';
+    } else if(element.length > 1) {
+        descriptionText = 'Got ' + element.length + ' elements'
+    }
+    return [element, descriptionText];
+}
+console.log(countAndDescribe('Hi, there!'));
--- dist/6-Generics/app.js
+++ dist/6-Generics/app.js
@@ -15,4 +15,15 @@ function merge(objA, objB) {
 }
 const mergedObj = merge({ name: 'Evanilson', hobbies: ['Sports'] }, { age: 23 });
 console.log(mergedObj.age);
+function countAndDescribe(element) {
+    let descriptionText = 'Got no value';
+    if (element.length === 1) {
+        descriptionText = 'Got 1 element';
+    }
+    else if (element.length > 1) {
+        descriptionText = 'Got ' + element.length + ' elements';
+    }
+    return [element, descriptionText];
+}
+console.log(countAndDescribe('Hi, there!'));
 //# sourceMappingURL=app.js.map
