c3b66e10f0356124b096cdae0e29c2dc66100acc
EvanilsonP <pevanilson99@gmail.com>
2022-11-17 14:35:55 -0300

    Generics

---
+++ dist/6-Generics/app.js
@@ -0,0 +1,12 @@
+"use strict";
+// const names: Array<string> = []; // string[];
+// names[0].split(' ');
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve(10);
+//     }, 2000);
+// });
+// promise.then(data => {
+//     // data.split(' ');
+// });
+//# sourceMappingURL=app.js.map
